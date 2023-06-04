2c89c451a24e1b5292443db0ce0dcc773b212e18
meriem-bouyemmout <116169146+meriem-bouyemmout@users.noreply.github.com>
2023-06-04 11:09:44 +0200

    Update stock1.py

--- stock1.py
+++ stock1.py
@@ -100,6 +100,145 @@ def AjouterArticle():
 # ajouter_article.pack()
 
 
+def Ajouter_BR():
+    add_BR_window = tk.Toplevel()
+    add_BR_window.title("Ajouter BR")
+    add_BR_window.geometry("1300x700")
+    
+    # Define the labels and entry fields
+    code_br_label = ttk.Label(add_BR_window, text="Code BR:")
+    code_br_entry = ttk.Entry(add_BR_window)
+    date_label = ttk.Label(add_BR_window, text="date:")
+    date_entry = ttk.Entry(add_BR_window)
+    nom_label = ttk.Label(add_BR_window, text="Nom :")
+    nom_entry = ttk.Entry(add_BR_window)
+    prenom_label = ttk.Label(add_BR_window, text="Prénom:")
+    prenom_entry = ttk.Entry(add_BR_window)
+
+
+
+    
+    code_br_label.grid(row=0, column=0, padx=10, pady=10)
+    code_br_entry.grid(row=0, column=1, padx=10, pady=10)
+    date_label.grid(row=1, column=0, padx=10, pady=10)
+    date_entry.grid(row=1, column=1, padx=10, pady=10)
+    nom_label.grid(row=2, column=0, padx=10, pady=10)
+    nom_entry.grid(row=2, column=1, padx=10, pady=10)
+    prenom_label.grid(row=3, column=0, padx=10, pady=10)
+    prenom_entry.grid(row=3, column=1, padx=10, pady=10)
+
+
+
+    def save_br():
+
+        conn = sqlite3.connect('stock.db')
+        c = conn.cursor()
+
+
+    
+        code_br = code_br_entry.get()
+        date = date_entry.get()
+        nom = nom_entry.get()
+        prenom = prenom_entry.get()
+        
+        
+
+        # Insert the new article into the database
+        c.execute("INSERT INTO BR VALUES (?, ?, ?, ?)",
+                  (code_br,date,nom,prenom))
+
+
+        code_br_entry.delete(0, END)
+        date_entry.delete(0, END)
+        nom_entry.delete(0, END)
+        prenom_entry.delete(0, END)
+
+
+
+       
+        conn.commit()
+
+        
+
+    
+    save_button = ttk.Button(add_BR_window, text="Save", command=save_br)
+    save_button.grid(row=6, column=1, padx=10, pady=10, columnspan=2)
+
+
+def Ajouter_BSM():
+    add_BSM_window = tk.Toplevel()
+    add_BSM_window.title("Ajouter BSM")
+    add_BSM_window.geometry("1300x700")
+    
+    # Define the labels and entry fields
+    code_bsm_label = ttk.Label(add_BSM_window, text="Code BSM:")
+    code_bsm_entry = ttk.Entry(add_BSM_window)
+    date_label = ttk.Label(add_BSM_window, text="date:")
+    date_entry = ttk.Entry(add_BSM_window)
+    nom_label = ttk.Label(add_BSM_window, text="Nom :")
+    nom_entry = ttk.Entry(add_BSM_window)
+    prenom_label = ttk.Label(add_BSM_window, text="Prénom:")
+    prenom_entry = ttk.Entry(add_BSM_window)
+
+
+
+    
+    code_bsm_label.grid(row=0, column=0, padx=10, pady=10)
+    code_bsm_entry.grid(row=0, column=1, padx=10, pady=10)
+    date_label.grid(row=1, column=0, padx=10, pady=10)
+    date_entry.grid(row=1, column=1, padx=10, pady=10)
+    nom_label.grid(row=2, column=0, padx=10, pady=10)
+    nom_entry.grid(row=2, column=1, padx=10, pady=10)
+    prenom_label.grid(row=3, column=0, padx=10, pady=10)
+    prenom_entry.grid(row=3, column=1, padx=10, pady=10)
+
+
+
+    def save_bsm():
+
+        conn = sqlite3.connect('stock.db')
+        c = conn.cursor()
+
+
+    
+        code_bsm = code_bsm_entry.get()
+        date = date_entry.get()
+        nom = nom_entry.get()
+        prenom = prenom_entry.get()
+        
+        
+
+        # Insert the new article into the database
+        c.execute("INSERT INTO BSM VALUES (?, ?, ?, ?)",
+                  (code_bsm,date,nom,prenom))
+
+
+        code_bsm_entry.delete(0, END)
+        date_entry.delete(0, END)
+        nom_entry.delete(0, END)
+        prenom_entry.delete(0, END)
+
+
+
+       
+        conn.commit()
+
+        
+
+    
+    save_button = ttk.Button(add_BSM_window, text="Save", command=save_bsm)
+    save_button.grid(row=6, column=1, padx=10, pady=10, columnspan=2)
+
+
+
+
+    def annuler():
+        add_BSM_window.destroy()
+
+
+    exit_button = ttk.Button(add_BSM_window, text="Retour", command=annuler)
+    exit_button.grid(row=7, column=1, padx=10, pady=10, columnspan=2)
+
 
 def show_articles():
 
@@ -163,6 +302,53 @@ def show_articles():
 # afficher_article.pack()
 
 
+def show_br():
+
+
+    conn = sqlite3.connect('stock.db')
+    c = conn.cursor()
+    
+    
+    BR_window = tk.Toplevel()
+    BR_window.title("List BR")
+    BR_window.geometry("1300x700")
+
+    # Create a treeview widget to display the articles
+    BR_treeview = ttk.Treeview(BR_window, columns=("date", "nom", "prénom"))
+    BR_treeview.heading("#0", text="Code BR")
+    BR_treeview.heading("date", text="Date")
+    BR_treeview.heading("nom", text="Nom")
+    BR_treeview.heading("prénom", text="Prénom")
+    BR_treeview.pack(fill="both", expand=True)
+
+    BR_treeview.column("#0", width=100, stretch=NO)
+    BR_treeview.column("date", width=100, stretch=NO)
+    BR_treeview.column("nom", width=100, stretch=NO)
+    BR_treeview.column("prénom", width=100, stretch=NO)
+   
+
+    
+    c.execute("""SELECT * FROM BR""")
+    BR = c.fetchall()
+
+    
+    for BR in BR:
+        code_br = BR[0]
+        date = BR[1]
+        nom = BR[2]
+        prenom = BR[3]
+        
+        BR_treeview.insert("", "end", text=code_br, values=(date, nom, prenom))
+
+
+
+# afficher_article = Button(root, text='Liste Articles', command=show_articles)
+
+
+
+
+
+
 def article_details():
     detail_window = tk.Toplevel()
     detail_window.title("Detail Article")
@@ -183,6 +369,7 @@ def article_details():
 
         conn = sqlite3.connect('stock.db')
         c = conn.cursor()
+        s = conn.cursor()
         
 
 
@@ -195,11 +382,12 @@ def article_details():
 
         
         c.execute("""SELECT * FROM ENTRES WHERE ENTRES.code_article=?""", (code_art,))
-
-
+             
         entres=c.fetchall()
 
+        s.execute("""SELECT * FROM SORTIES WHERE SORTIES.code_article=?""", (code_art,)) 
 
+        sorties=s.fetchall()       
 
         entres_treeview = ttk.Treeview(detail_window, columns=("qté_entres", "prix_ent", "montant_ent", "code_br"))
         entres_treeview.heading("#0", text="code entré")
@@ -209,7 +397,13 @@ def article_details():
         entres_treeview.heading("code_br", text="code br")
         entres_treeview.pack(fill="both", expand=True)
 
-    
+        sorties_treeview = ttk.Treeview(detail_window, columns=("qté_sort", "prix_sort", "montant_sort", "code_bs"))
+        sorties_treeview.heading("#0", text="code sorties")
+        sorties_treeview.heading("qté_sort", text="qté sorties")
+        sorties_treeview.heading("prix_sort", text="prix sorties")
+        sorties_treeview.heading("montant_sort", text="montant sorties")
+        sorties_treeview.heading("code_bs", text="code bs")
+        sorties_treeview.pack(fill="both", expand=True)
         
 
     
@@ -225,20 +419,16 @@ def article_details():
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+        for sortie in sorties :
+            code_sort = sortie[0]
+            qté_sort = sortie[1]
+            prix_sort = sortie[2]
+            montant_sort = sortie[3]
+            code_bs = sortie[4]
         
+            sorties_treeview.insert("", "end", text=code_sort, values=(qté_sort, prix_sort, montant_sort, code_bs))
+
+
        
         
 
@@ -297,7 +487,7 @@ def AjouterEntres():
 
 
 
-    def save_sorties():
+    def save_entres():
 
         conn = sqlite3.connect('stock.db')
         c = conn.cursor()
@@ -323,11 +513,11 @@ def AjouterEntres():
 
         c.execute("""
         UPDATE ARTICLES
-        SET qte = qte + (SELECT qte_ent FROM ENTRES WHERE ENTRES.code_article = ARTICLES.code_art),
-        prix = (SELECT prix_ent FROM ENTRES WHERE ENTRES.code_article = ARTICLES.code_art),
-        montant = (qte + (SELECT qte_ent FROM ENTRES WHERE ENTRES.code_article = ARTICLES.code_art))*(SELECT prix_ent FROM ENTRES WHERE ENTRES.code_article = ARTICLES.code_art)
+        SET qte = qte + (SELECT qte_ent FROM ENTRES WHERE ENTRES.code_entre = ?),
+        prix = (SELECT prix_ent FROM ENTRES WHERE ENTRES.code_entre = ?),
+        montant = (qte + (SELECT qte_ent FROM ENTRES WHERE ENTRES.code_entre = ?))*(SELECT prix_ent FROM ENTRES WHERE ENTRES.code_entre = ?)
         WHERE code_art = ?
-        """, (code_article,))
+        """, (code_entre,code_entre,code_entre,code_entre,code_article,))
 
 
 
@@ -353,7 +543,7 @@ def AjouterEntres():
         
 
     
-    save_button = ttk.Button(add_entres_window, text="Save", command=save_sorties)
+    save_button = ttk.Button(add_entres_window, text="Save", command=save_entres)
     save_button.pack()
 
     def annuler():
@@ -376,31 +566,31 @@ def AjouterSorties():
     # Define the labels and entry fields
     code_sortie_label = ttk.Label(add_sorties_window, text="Code Sortie:")
     code_sortie_entry = ttk.Entry(add_sorties_window)
-    qté_ent_label = ttk.Label(add_sorties_window, text="Quantité Sortie:")
-    qté_ent_entry = ttk.Entry(add_sorties_window)
-    prix_ent_label = ttk.Label(add_sorties_window, text="Prix U Entré:")
-    prix_ent_entry = ttk.Entry(add_sorties_window)
-    code_br_label = ttk.Label(add_sorties_window, text="Code BSM:")
-    code_br_entry = ttk.Entry(add_sorties_window)
+    qté_sort_label = ttk.Label(add_sorties_window, text="Quantité Sortie:")
+    qté_sort_entry = ttk.Entry(add_sorties_window)
+    prix_sort_label = ttk.Label(add_sorties_window, text="Prix U Sortie:")
+    prix_sort_entry = ttk.Entry(add_sorties_window)
+    code_bs_label = ttk.Label(add_sorties_window, text="Code BSM:")
+    code_bs_entry = ttk.Entry(add_sorties_window)
     code_article_label = ttk.Label(add_sorties_window, text="Code Article:")
     code_article_entry = ttk.Entry(add_sorties_window)
 
     # Add the labels and entry fields to the window
     code_sortie_label.pack()
     code_sortie_entry.pack()
-    qté_ent_label.pack()
-    qté_ent_entry.pack()
-    prix_ent_label.pack()
-    prix_ent_entry.pack()
-    code_br_label.pack()
-    code_br_entry.pack()
+    qté_sort_label.pack()
+    qté_sort_entry.pack()
+    prix_sort_label.pack()
+    prix_sort_entry.pack()
+    code_bs_label.pack()
+    code_bs_entry.pack()
     code_article_label.pack()
     code_article_entry.pack()
     
 
 
 
-    def save_entres():
+    def save_sorties():
 
         conn = sqlite3.connect('stock.db')
         c = conn.cursor()
@@ -408,18 +598,18 @@ def AjouterSorties():
 
 
     
-        code_entre = code_sortie_entry.get()
-        qté_ent = qté_ent_entry.get()
-        prix_ent = prix_ent_entry.get()
-        code_br = code_br_entry.get()
+        code_sort = code_sortie_entry.get()
+        qté_sort = qté_sort_entry.get()
+        prix_sort = prix_sort_entry.get()
+        code_bs = code_bs_entry.get()
         code_article = code_article_entry.get()
 
         
-        montant_ent = float(prix_ent) * float(qté_ent)
+        montant_sort = float(prix_sort) * float(qté_sort)
 
         
         c.execute("INSERT INTO SORTIES VALUES (?, ?, ?, ?, ?, ?)",
-                  (code_entre, qté_ent, prix_ent, montant_ent, code_br, code_article))
+                  (code_sort, qté_sort, prix_sort, montant_sort, code_bs, code_article))
 
 
 
@@ -430,7 +620,7 @@ def AjouterSorties():
         prix = (SELECT prix_sort FROM SORTIES WHERE SORTIES.code_sortie = ?),
         montant = (qte - (SELECT qte_sort FROM SORTIES WHERE SORTIES.code_sortie = ?))*(SELECT prix_sort FROM SORTIES WHERE SORTIES.code_sortie = ?)
         WHERE code_art = ?
-        """, (code_entre,code_entre,code_entre,code_entre,code_article,))
+        """, (code_sort,code_sort,code_sort,code_sort,code_article,))
 
 
 
@@ -442,9 +632,9 @@ def AjouterSorties():
 
 
         code_sortie_entry.delete(0, END)
-        qté_ent_entry.delete(0, END)
-        prix_ent_entry.delete(0, END)
-        code_br_entry.delete(0, END)
+        qté_sort_entry.delete(0, END)
+        prix_sort_entry.delete(0, END)
+        code_bs_entry.delete(0, END)
         code_article_entry.delete(0, END)
         
 
@@ -456,7 +646,7 @@ def AjouterSorties():
         
 
     
-    save_button = ttk.Button(add_sorties_window, text="Save", command=save_entres)
+    save_button = ttk.Button(add_sorties_window, text="Save", command=save_sorties)
     save_button.pack()
 
     def annuler():
@@ -487,7 +677,6 @@ def login():
         ajouter_article.config(bg="yellow", fg="white", font=("Arial", 16))
         ajouter_article.pack()
 
-
         afficher_article = Button(options_window, text='Liste Articles', command=show_articles)
         afficher_article.config(bg="green", fg="white", font=("Arial", 16))
         afficher_article.pack()
@@ -504,6 +693,17 @@ def login():
         ajouter_Sortie.config(bg="pink", fg="white", font=("Arial", 16))
         ajouter_Sortie.pack()
      
+        ajouter_br = Button(options_window, text='Ajouter BR ', command=Ajouter_BR)
+        ajouter_br.config(bg="black", fg="white", font=("Arial", 16))
+        ajouter_br.pack()     
+
+        ajouter_bsm = Button(options_window, text='Ajouter BSM ', command=Ajouter_BSM)
+        ajouter_bsm.config(bg="black", fg="white", font=("Arial", 16))
+        ajouter_bsm.pack()
+
+        afficher_br = Button(options_window, text='List BR ', command=show_br)
+        afficher_br.config(bg="brown", fg="white", font=("Arial", 16))
+        afficher_br.pack()
 
     else:
         # Show an error message if the username or password is incorrect
